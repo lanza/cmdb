64c200c625790e051b58e59125554e1478f77966
Nathan Lanza <nathan@lanza.io>
2019-01-09 21:57:27 -0800

    Add variable querying to vim plugin and cmdb

--- rplugin/python3/cmdb.py
+++ rplugin/python3/cmdb.py
@@ -3,6 +3,13 @@
 import socket
 import sys
 import pynvim
+from enum import Enum
+import _thread
+
+class IDEType(Enum):
+    Error = 0
+    FileAndLocation = 1
+    VariableEvaluation = 2
 
 
 def uint32_from_bytes(data):
@@ -25,28 +32,75 @@ def read_string_at(data):
 def read_uint32_at(data):
     return (uint32_from_bytes(data[0:3]), data[4:])
 
+def encode_unsigned(i: int) -> bytes:
+    return i.to_bytes(4, byteorder="little")
+
+def encode_string(s: str) -> bytes:
+    length = len(s)
+    data = encode_unsigned(length) + s.encode('ascii')
+    return data
+
+def encode_info(*args):
+    d = bytes()
+    for arg in args:
+        t = type(arg)
+        if t is int:
+            d += encode_unsigned(arg)
+        elif t is str:
+            d += encode_string(arg)
+        else:
+            print(f"Bad encode_info: {arg} {t}")
+
+    return d
+
+
+VIM = None
+
+def adjust_line(*args):
+    VIM.command(f"e {args[1]}")
+    VIM.command(f"normal {args[0]}gg")
+
+
+def echo_variable(*args):
+    VIM.command(f"echom \"{args[0]}\"")
+
+
+def listen_main(name, s, vim):
+    while True:
+        data = s.recv(8)
+        if len(data) == 0:
+            return
+        t, data = read_uint32_at(data)
+        size, data = read_uint32_at(data)
+        data = s.recv(size)
+
+        if t == IDEType.FileAndLocation.value:
+            line, data = read_uint32_at(data)
+            file, data = read_string_at(data)
+            vim.async_call(adjust_line, line, file)
+        elif t == IDEType.VariableEvaluation.value:
+            value, data = read_string_at(data)
+            vim.async_call(echo_variable, value)
 
 @pynvim.plugin
 class Main(object):
     def __init__(self, vim: pynvim.api.nvim.Nvim):
+        global VIM
+        VIM = vim
         self.vim = vim
+
 
     @pynvim.command('CMDBListen', nargs='1')
     def cmdb_listen(self, *args):
-        self.main(args)
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.connect(("127.0.0.1", int(args[0][0])))
+        _thread.start_new_thread(listen_main, ("listen", self.s, self.vim))
 
-    def main(self, args):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect(("127.0.0.1", int(args[0][0])))
-            while True:
-                data = s.recv(4)
-                if len(data) == 0:
-                    return
-                size, data = read_uint32_at(data)
-                data = s.recv(size)
-                line, data = read_uint32_at(data)
-                file, data = read_string_at(data)
 
-                self.vim.command(f"e {file}")
-                self.vim.command(f"normal {line}gg")
+    @pynvim.command('CMDBExpression', nargs='1')
+    def cmdb_expression(self, *args):
+        self.s.send(encode_unsigned(len(args[0][0]) + 4))
+        self.s.send(encode_info(str(args[0][0])))
+
+
 
